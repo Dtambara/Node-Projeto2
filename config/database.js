30858015bc0b84139b524125a676ffa613a2a178
dtambara <danilo.tambara.2011@gmail.com>
2018-04-27 17:44:42 -0300

    mudança no database.js para base do heroku

--- config/database.js
+++ config/database.js
@@ -1,7 +1,8 @@
 module.exports = {
-  username: 'root',
-  database: 'SNIPPETFY',
-  host: 'localhost',
+  username: 'lilvozun252xhqg6',
+  password: 'za7um5tpheiuup04',
+  database: 'xkk5l79hfps366lm',
+  host: 'h7xe2knj2qb6kxal.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
   dialect: 'mysql',
   port: '3306',
 };
